f00980d1ef70b8ae2f301d0896f79ab452e65eb1
tmszny <tomasz.matuszny@gmail.com>
2024-01-04 14:44:05 +0100

    testy konsoli

---
+++ cloud/src/main/java/tms/cloud/web/OrderController.java
@@ -0,0 +1,53 @@
+package tms.cloud.web;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import tms.cloud.Order;
+import tms.cloud.User;
+import tms.cloud.data.OrderRepository;
+
+@Controller
+@Slf4j
+@RequestMapping("/orders")
+@SessionAttributes("order")
+public class OrderController {
+
+    private OrderRepository orderRepo;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping("/current")
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
+
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        order.setUser(user);
+        orderRepo.save(order);
+        sessionStatus.setComplete();
+        log.info("Zamówienie zostało złożone: " + order);
+
+        return "redirect:/";
+    }
+}
